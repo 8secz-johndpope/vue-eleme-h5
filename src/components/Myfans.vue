d90d4c003fca524c0bd8f747c15c2625535188cd
calabasher <809136696@qq.com>
2019-01-29 16:36:18 +0800

    增加文件

---
+++ src/components/Myfans.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <van-swipe-cell :right-width="65">
+      <van-cell-group>
+        <van-cell title="单元格" value="内容" />
+      </van-cell-group>
+      <span slot="right">删除00</span>
+    </van-swipe-cell>
+    <van-swipe-cell :right-width="65" :on-close="onClose">
+      <van-cell-group>
+        <van-cell title="单元格" value="内容" />
+      </van-cell-group>
+      <span slot="right">删除</span>
+    </van-swipe-cell>
+    <Fixednav></Fixednav>
+  </div>
+</template>
+
+<script>
+
+  import { Dialog } from "vant";
+  import Fixednav from './small_components/Fixed_nav';
+
+import "../css/common.css";
+export default {
+  components:{
+         Fixednav,
+         // [Swipe.name]:Swipe,
+         // [SwipeCell.name]:SwipeCell,
+         // [CellGroup.name]:CellGroup,
+         // [Cell.name]:Cell,
+         [Dialog.name]:Dialog,
+      },
+  name: 'myfans',
+  data () {
+    return {
+      msg: '1'
+    };
+  },
+  mounted () {
+
+  },
+  computed: {
+
+  },
+  methods: {
+    onClose (clickPosition, instance) {
+      switch (clickPosition) {
+        case 'left':
+        case 'cell':
+        case 'outside':
+          instance.close();
+          break;
+        case 'right':
+          Dialog.confirm({
+            message: '确定删除吗？'
+          }).then(() => {
+            instance.close();
+          });
+          break;
+      }
+    }
+  }
+}
+</script>
+<style lang="css" scoped>
+</style>
--- src/components/small_components/Fixed_nav.vue
+++ src/components/small_components/Fixed_nav.vue
@@ -3,8 +3,8 @@
 
   <van-tabbar v-model="active">
     <van-tabbar-item icon="home-o">标签</van-tabbar-item>
-    <van-tabbar-item icon="search" dot>标签</van-tabbar-item>
-    <van-tabbar-item icon="friends-o" info="5">标签</van-tabbar-item>
+    <van-tabbar-item icon="search" dot :to="'/business/s10001'">商家</van-tabbar-item>
+    <van-tabbar-item icon="friends-o" info="5" :to="'/Myfans'">粉丝</van-tabbar-item>
     <van-tabbar-item icon="setting-o" info="20">标签</van-tabbar-item>
   </van-tabbar>
 </template>
