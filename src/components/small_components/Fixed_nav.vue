64c3f20263d053496f930a0028f2d93f8067d91d
calabasher <809136696@qq.com>
2019-02-19 17:13:13 +0800

    发表内容 -- 一键发表

--- src/components/small_components/Fixed_nav.vue
+++ src/components/small_components/Fixed_nav.vue
@@ -11,15 +11,40 @@
       <div class="closePop">
         <van-icon name="close" @click="closePop" />
       </div>
-      <div class="flex-center selectConverstionType">
+      <div class="selectConverstionType">
         <!-- <van-button plain type="primary">一键对话</van-button>
         <van-button plain type="danger">分段对话</van-button> -->
         <van-tabs type="card">
-          <van-tab title="一键对话">
-            <textarea>00</textarea>
+          <van-tab title="一键对话" class="placehoder-setting">
+            <van-cell-group class="tabContents">
+              <van-field
+                v-model="message"
+                type="textarea"
+                placeholder="写标准的内容并使用合适的话题，能让更多的人看到"
+                rows="4"
+                autosize
+              />
+            </van-cell-group>
           </van-tab>
           <van-tab title="分段对话">分段对话</van-tab>
         </van-tabs>
+      </div>
+      <div class="theme-type">
+        <van-cell-group>
+          <van-field
+            v-model="theme"
+            type="textarea"
+            placeholder="请输入类型"
+            rows="1"
+            autosize
+          />
+        </van-cell-group>
+        <van-button size="mini">#类型</van-button>
+      </div>
+      <div class="spaceDiv"></div>
+      <div class="opt-zone">
+        <van-button type="default"><i class="fa fa-edit" aria-hidden="true"></i>草稿</van-button>
+        <van-button type="danger"><i class="fa fa-arrow-up" aria-hidden="true"></i>发表</van-button>
       </div>
     </van-popup>
   </div>
@@ -31,7 +56,9 @@ export default {
   data () {
     return {
       active: 0,
-      popupShow: false, 
+      popupShow: false,
+      message: '',
+      theme: '',
     };
   },
   mounted () {
@@ -69,6 +96,26 @@ export default {
   }
   .selectConverstionType{
     width: 100%;
-    height: 100%;
+    display: flex;
+    justify-content: center;
+    margin-top: 100px;
+  }
+  .theme-type{
+    width: 80%;
+    margin: 0 auto;
+    margin-top: 10px;
+    margin-bottom: 10px;
+  }
+  .tabContents{
+    margin-top: 15px;
+  }
+  .spaceDiv{
+    height: 10rem;
+  }
+  .opt-zone{
+    position: fixed;
+    bottom: 15px;
+    width: 100%;
+    text-align: center;
   }
 </style>
