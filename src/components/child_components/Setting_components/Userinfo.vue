a938f05ce8e078ee1dba9f272f0d6469ee303f73
calabasher <809136696@qq.com>
2019-02-25 12:15:16 +0800

    用户设置 -- 名称、个性签名设置

--- src/components/child_components/Setting_components/Userinfo.vue
+++ src/components/child_components/Setting_components/Userinfo.vue
@@ -15,16 +15,44 @@
         </van-uploader>
       </template>
     </van-cell>
-    <van-cell title="名称" is-link value="全村的希望" />
+    <van-cell title="名称" is-link value="全村的希望" @click="openUserNameSettingPopup" />
     <van-cell title="性别" is-link value="男" @click="showSexPop" />
     <van-cell title="地区" is-link value="中国·广东·深圳" />
-    <van-cell title="签名" is-link value="做个俗人，贪财好色" />
+    <van-cell title="签名" is-link value="做个俗人，贪财好色" @click="openAutographSettingPopup" />
     <!-- 性别弹出层 -->
     <van-actionsheet
-      v-model="sexPopShow"
-      :actions="actions"
-      @select="onSelectSex"
+    v-model="sexPopShow"
+    :actions="actions"
+    @select="onSelectSex"
     />
+    <!-- 名称设置 -->
+    <van-popup v-model="userNameSettingShow" position="bottom" :overlay="true" class="grayBg">
+      <van-nav-bar
+        title="名称设置"
+        left-text="取消"
+        right-text="确认"
+        @click-left="cancelUpdateUserName"
+        @click-right="updateUserName"
+        class="grayBarBg"
+      />
+      <van-cell-group>
+        <van-field v-model="userName" placeholder="请输入用户名" clearable />
+      </van-cell-group>
+    </van-popup>
+    <!-- 个人签名设置 -->
+    <van-popup v-model="autographSettingShow" position="bottom" :overlay="true" class="grayBg">
+      <van-nav-bar
+        title="个性签名设置"
+        left-text="取消"
+        right-text="确认"
+        @click-left="cancelUpdateAutograph"
+        @click-right="updateAutograph"
+        class="grayBarBg"
+      />
+      <van-cell-group>
+        <van-field v-model="autograph" placeholder="请输入个性签名" clearable />
+      </van-cell-group>
+    </van-popup>
   </div>
 </template>
 <script>
@@ -36,6 +64,8 @@
       return {
         msg: '1',
         sexPopShow: false,  // 性别弹出层
+        userNameSettingShow: false,  // 名称设置弹框
+        autographSettingShow: false,  // 个性签名设置弹框
         actions: [
           {
             name: '男',
@@ -45,7 +75,9 @@
             name: '女',
             value: 1
           },
-        ]
+        ],
+        userName: 'lisan',  // 用户名
+        autograph: '做个俗人，贪财好色', // 个性签名
       };
     },
     mounted () {
@@ -68,9 +100,36 @@
         this.sexPopShow = false;
         this.$toast(item.name);
       },
+      // 上传文件
       onRead(file) {
         this.$toast('上传成功')
         console.log(file)
+      },
+      // 打开修改用户名弹框
+      openUserNameSettingPopup () {
+        this.userNameSettingShow = true;
+      },
+      // 取消修改用户名
+      cancelUpdateUserName () {
+        this.userNameSettingShow = false;
+      },
+      // 修改用户名
+      updateUserName () {
+        this.$toast('修改用户名成功')
+        this.userNameSettingShow = false;
+      },
+      // 打开修改个性签名弹框
+      openAutographSettingPopup () {
+        this.autographSettingShow = true;
+      },
+      // 取消修改个性签名
+      cancelUpdateAutograph () {
+        this.autographSettingShow = false;
+      },
+      // 修改个性签名
+      updateAutograph () {
+        this.$toast('修改个性签名成功')
+        this.autographSettingShow = false;
       }
     }
 }
