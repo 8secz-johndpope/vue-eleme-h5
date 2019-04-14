e92124e5932830eb3a29b52f1dbd96d3f6f25a09
calabasher <809136696@qq.com>
2019-04-14 16:49:41 +0800

    聊天表情包测试

--- src/api/api.js
+++ src/api/api.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 export function getEmojiData() {
   return axios({
       method: 'get',
-      url: '/yiluxiangbei87110/vue-chat/blob/master/static/emojiDB.json',
+      url: '/static/emojiDB.json',
     })
     .then(function(res) {
       return Promise.resolve(res.data);
