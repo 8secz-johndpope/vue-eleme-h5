32848258873a1f17aac193a5040c605c9966aaad
calabasher <809136696@qq.com>
2019-04-16 12:29:53 +0800

    速推计划 -- 达人粉丝

--- src/components/child_components/Extension_components/Daren.vue
+++ src/components/child_components/Extension_components/Daren.vue
@@ -2,43 +2,38 @@
   <div>
     <div class="daren-content-box">
       <div class="cell-item">
-        地区（单选）
-        <van-button size="mini" plain @click="darenSelectPop = true" type="danger" class="address-btn">{{address}}</van-button>
+        <div class="flex-column" @click="darenSelectPop = true">
+          <span class="van-avatar van-avatar-select">+</span>
+          <span class="van-avatar-name">添加</span>
+        </div>
+        <div class="flex-column hasSelectedUser" v-for="(item, index) in getImitateUserList">
+          <img src="https://avatars1.githubusercontent.com/u/34303195?s=460&v=4" class="van-avatar van-avatar-select" />
+          <span class="van-avatar-name">{{getImitateUserList[0].userName}}</span>
+          <van-icon name="close" class="close-icon" @click="deleteSelectUser(index)"/>
+        </div>
       </div>
     </div>
     <!--达人选择弹框 -->
     <van-popup v-model="darenSelectPop" position="bottom" :overlay="true">
-      <div class="flex-space-between">
-        <span>取消</span>
-        <span>头部</span>
-        <span>确认</span>
+      <div class="flex-space-between blue-color popTop">
+        <div @click="darenSelectPop = false">取消</div>
+        <div class="popTop-title">
+          <span>选择达人</span>
+          <span class="gray-color popTop-title-dec">建议选择5个达人以上</span>
+        </div>
+        <div @click="finishedSelect">完成({{totalSelect}}/20)</div>
       </div>
       <div>
         <van-search placeholder="请输入搜索关键词" v-model="searchValue" />
       </div>
-      <div class="flex-space-between">
+      <div class="select-zone">
         <div class="select-zone-left">
           <van-badge-group :active-key="activeKey" @change="onChangeBadge">
-            <van-badge title="标签名称" />
-            <van-badge title="标签名称" info="8" />
-            <van-badge title="标签名称" info="99" />
-            <van-badge title="标签名称" info="99+" />
+            <van-badge :title="item.badgeName" :info="item.badgeSelected > 0 ? item.badgeSelected : '' " v-for="(item, index) in badgeArrs" />
           </van-badge-group>
         </div>
         <div class="select-zone-right">
-          <van-checkbox-group v-model="checkboxResult">
-            <van-cell-group>
-              <van-cell
-                v-for="(item, index) in checkboxList"
-                clickable
-                :key="item"
-                :title="`复选框 ${item}`"
-                @click="toggle(index)"
-                >
-                <van-checkbox :name="item" ref="checkboxes" />
-              </van-cell>
-            </van-cell-group>
-          </van-checkbox-group>
+          <SelectUserCheckbox :checkboxArr = "getImitateUserList"></SelectUserCheckbox>
         </div>
       </div>
     </van-popup>
@@ -46,6 +41,7 @@
 </template>
 
 <script>
+  import SelectUserCheckbox from './SelectUserCheckbox';
   import Vue from 'vue';
   import { mapGetters } from 'vuex';
   import { Badge, BadgeGroup } from 'vant';
@@ -53,16 +49,78 @@
   Vue.use(BadgeGroup);
   export default {
     components:{
+      SelectUserCheckbox,
     },
     name: 'daren',
     data () {
       return {
         searchValue: '',  // 搜索内容
-        darenSelectPop: true, // 达人选择弹框
+        darenSelectPop: false, // 达人选择弹框
         address: '广东省·深圳市·罗湖区', // 地址
         activeKey: 0,
-        checkboxList: ['a', 'b', 'c'],
-        checkboxResult: ['a', 'b']
+        badgeArrs: [
+          {
+            badgeType: '0001',  // 类型
+            badgeName: '服装',  // 标签名称
+            badgeSelected: 2,  // 标签已选数量
+          },
+          {
+            badgeType: '0001',  // 类型
+            badgeName: '动漫',  // 标签名称
+            badgeSelected: 2,  // 标签已选数量
+          },
+          {
+            badgeType: '0001',  // 类型
+            badgeName: '奢侈品',  // 标签名称
+            badgeSelected: 1,  // 标签已选数量
+          },
+          {
+            badgeType: '0001',  // 类型
+            badgeName: '母婴',  // 标签名称
+            badgeSelected: 0,  // 标签已选数量
+          },
+          {
+            badgeType: '0001',  // 类型
+            badgeName: '服装',  // 标签名称
+            badgeSelected: 0,  // 标签已选数量
+          },
+          {
+            badgeType: '0001',  // 类型
+            badgeName: '动漫',  // 标签名称
+            badgeSelected: 0,  // 标签已选数量
+          },
+          {
+            badgeType: '0001',  // 类型
+            badgeName: '搞笑',  // 标签名称
+            badgeSelected: 0,  // 标签已选数量
+          },
+          {
+            badgeType: '0001',  // 类型
+            badgeName: '游戏',  // 标签名称
+            badgeSelected: 0,  // 标签已选数量
+          },
+          {
+            badgeType: '0001',  // 类型
+            badgeName: '帅哥',  // 标签名称
+            badgeSelected: 0,  // 标签已选数量
+          },
+          {
+            badgeType: '0001',  // 类型
+            badgeName: '数码',  // 标签名称
+            badgeSelected: 0,  // 标签已选数量
+          },
+          {
+            badgeType: '0001',  // 类型
+            badgeName: '美妆',  // 标签名称
+            badgeSelected: 0,  // 标签已选数量
+          },
+          {
+            badgeType: '0001',  // 类型
+            badgeName: '交通',  // 标签名称
+            badgeSelected: 0,  // 标签已选数量
+          },
+        ],
+        currentRate: 100, // 当前进度
       };
     },
     mounted () {
@@ -70,21 +128,28 @@
     computed: {
       ...mapGetters([
         'getAreaList',
-      ])
+        'getImitateUserList', // 获取用户列表数据
+      ]),
+      totalSelect: function () {
+        let sum = 0;
+        this.badgeArrs.forEach( (val, index) => {
+          sum += val.badgeSelected;
+        })
+        return sum
+      }
     },
     methods: {
-      // 确认地址
-      confirmArea (e) {
-        this.darenSelectPop = false;
-        this.address = e[0].name+'·'+e[1].name+'·'+e[2].name;
-      },
       // 切换徽章
       onChangeBadge(key) {
         this.activeKey = key;
       },
-      // 复选框
-      toggle(index) {
-        this.$refs.checkboxes[index].toggle();
+      // 完成选择
+      finishedSelect () {
+        this.darenSelectPop = false;
+      },
+      // 删除已经选择的达人
+      deleteSelectUser (index) {
+        this.$toast('删除操作');
       }
     }
   }
@@ -95,14 +160,59 @@
     padding: 0 0.4rem;
   }
   .cell-item{
-    height: 0.7rem;
+    height: 1.6rem;
     display: flex;
     align-items: center;
+    text-align: center;
+  }
+  .van-avatar-select{
+    font-size: 0.533333rem;
+    background: #D4BBA7;
+    margin: 0 0.4rem 0.1rem 0;
+  }
+  .van-avatar-name{
+    max-width: 1.066667rem;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+  }
+  .hasSelectedUser{
+    position: relative;
+  }
+  .close-icon{
+    position: absolute;
+    top: 0;
+    right: 0.266667rem;
+    font-size: 0.373333rem;
+  }
+  .popTop{
+    font-size: 0.373333rem;
+    height: 1rem;
+    align-items: center;
+    padding: 0.133333rem 0.4rem;
+  }
+  .popTop-title{
+    font-size: 0.426667rem;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    color: #333;
+  }
+  .popTop-title-dec{
+    font-size: 0.32rem;
+  }
+  .select-zone{
+    display: flex;
+    justify-content: space-between;
   }
   .select-zone-left{
     width: 30%;
+    height: 6.133333rem;
+    overflow: scroll;
   }
   .select-zone-right{
     width: 70%;
+    height: 6.133333rem;
+    overflow: scroll;
   }
 </style>
---
+++ src/components/child_components/Extension_components/SelectUserCheckbox.vue
@@ -0,0 +1,81 @@
+<template>
+  <div>
+    <van-checkbox-group v-model="checkboxResult">
+      <van-cell-group>
+        <van-cell
+          v-for="(item, index) in checkboxList"
+          clickable
+          :key="item"
+          @click="toggle(index)"
+          class="checkbox-cell"
+          >
+          <template slot="title">
+            <div class="tleft van-user-item-left">
+              <div class="van-avatar-large"><img :src="checkboxArr[0].userImg" alt="" class="van-avatar-img"></div>
+              <div class="flex-column van-user-item-text-info">
+                <span class="van-userName">{{checkboxArr[0].userName}}</span>
+                <span>粉丝：{{COMMONFUNC.formatterW(checkboxArr[0].fansNum)}}</span>
+                <span class="van-autograph">{{checkboxArr[0].autograph}}</span>
+              </div>
+            </div>
+          </template>
+          <van-checkbox :name="item" ref="checkboxes" class="van-user-item-right"/>
+        </van-cell>
+      </van-cell-group>
+    </van-checkbox-group>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      // 复选框内容
+      'checkboxArr':{
+        type: Array,
+        default: [],
+      }
+    },
+    components:{
+    },
+    name: 'selectUserCheckbox',
+    data () {
+      return {
+        checkboxList: ['数码', '美妆', '音乐', '教育', '交通'],
+        checkboxResult: ['数码', '美妆'],
+      };
+    },
+    mounted () {
+    },
+    computed: {
+    },
+    methods: {
+      // 复选框
+      toggle(index) {
+        this.$refs.checkboxes[index].toggle();
+      }
+    }
+  }
+</script>
+<style lang="css" scoped>
+  .checkbox-cell{
+    display: flex;
+    justify-content: space-between;
+  }
+  .van-user-item-left{
+    display: flex;
+    align-items: center;
+    width: 170%;
+  }
+  .van-user-item-right{
+    display: flex;
+    height: 100%;
+    align-items: center;
+    justify-content: flex-end;
+  }
+  .van-autograph{
+    max-width: 150px;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+  }
+</style>
