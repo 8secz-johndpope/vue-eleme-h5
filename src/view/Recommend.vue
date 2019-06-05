aa7d4911fbcc759b0d936c278abe6088a29f63a5
ganluhua <je04@jingshukj.com.cn>
2019-06-05 17:00:24 +0800

    推荐页

--- src/view/Recommend.vue
+++ src/view/Recommend.vue
@@ -15,16 +15,32 @@
         <van-tab title="推荐">
           <PostCard :composition="getImitatePostList" class="item-box"></PostCard>
         </van-tab>
-        <van-tab title="话术">
-          <Conversation :composition="getImitateConversation" class="item-box"></Conversation>
+        <van-tab title="关注">
+          <PostCard :composition="getImitatePostList" class="item-box"></PostCard>
         </van-tab>
         <van-tab title="视频">
+          <Conversation :composition="getImitateConversation" class="item-box"></Conversation>
+        </van-tab>
+        <van-tab title="话术">
           <Conversation :composition="getImitateConversation" class="item-box"></Conversation>
         </van-tab>
         <van-tab title="文章">
           <articleList :composition="getImitateArticleList" class="item-box"></articleList>
         </van-tab>
       </van-tabs>
+      <div class="flex-center moreTabs" @click="allTabsShow = !allTabsShow">
+        <van-icon name="arrow-up" v-show="allTabsShow" />
+        <van-icon name="arrow-down" v-show="!allTabsShow" />
+      </div>
+      <div class="allTabs" v-show="allTabsShow">
+        <ul>
+          <li v-for="(item, index) in recommendMenuList" :key="item.containerid">
+            <van-button :class=" index === activeTab ? 'recommendMenu-btn-active' : '' " type="default" size="small" class="recommendMenu-btn" @click="selectTab(item,index)">
+              {{item.name}}
+            </van-button>
+          </li>
+        </ul>
+      </div>
     </div>
     <!-- 撑开Fixednav挡住的位置 -->
     <div class="space"></div>
@@ -45,6 +61,29 @@ export default {
       uname: '',
       activeTab: 0,
       keywords: '', // 搜索关键词
+      allTabsShow: false, // 所有tabs
+      recommendMenuList: [
+        {
+          name: '推荐',
+          containerid: 0, // 内容分类id
+        },
+        {
+          name: '关注',
+          containerid: 1, // 内容分类id
+        },
+        {
+          name: '视频',
+          containerid: 2, // 内容分类id
+        },
+        {
+          name: '话术',
+          containerid: 3, // 内容分类id
+        },
+        {
+          name: '文章',
+          containerid: 4, // 内容分类id
+        },
+      ]
     };
   },
   mounted () {
@@ -61,12 +100,18 @@ export default {
   methods: {
     changeTab(index, title) {
       this.$store.dispatch('setRcommendHighLightTab', index);
+      this.allTabsShow = false;
     },
     onSearch () {
       if (!this.keywords) {this.$toast('请输入搜索关键词'); return}
       this.$store.dispatch('setKeywords', this.keywords);
       this.$router.push('/result/'+this.keywords);
     },
+    // 菜单选择
+    selectTab (item, index) {
+      this.activeTab = index;
+      this.allTabsShow = false;
+    }
   },
   components: {
     Fixednav,
@@ -84,5 +129,41 @@ export default {
     z-index: 999;
     widtH: 100%;
     top: 0 !important;
+  }
+  .moreTabs{
+    position: fixed;
+    top: 1.4rem;
+    right: 0;
+    width: 50px;
+    height: 1rem;
+    z-index: 3;
+    background-color: #fff;
+  }
+  .allTabs{
+    position: fixed;
+    top: 2.5rem;
+    left: 0;
+    right: 0;
+    text-align: left;
+    padding: 10px 0.426667rem 5px 0.426667rem;
+    color: #333;
+    background-color: #fafafa;
+    z-index: 3;
+  }
+  .allTabs ul{
+    display: flex;
+    flex-wrap: wrap;
+  }
+  .allTabs ul li{
+    width: 25%;
+    text-align: center;
+    margin: 0 0 10px 0;
+  }
+  .recommendMenu-btn{
+    background-color: #eee;
+    width: 90%;
+  }
+  .recommendMenu-btn-active{
+    color: #ff8200;
   }
 </style>
